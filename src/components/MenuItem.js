721877776499ee7fb54fb85fee8a71f0cb2e29ef
root <dave.bach@iridium.com>
2025-01-26 06:33:17 +0000

    reverting to not using a place holder

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,48 +1,24 @@
-import React, { useState, useContext } from 'react';
-import './MenuItem.css';
+import React, { useEffect, useState } from 'react';
+import './MenuItem.css'
 import { Link, useParams } from 'react-router-dom';
-import { UserContext } from '../App.js';
-import placeholderImage from '../placeHolder.png';
 
 const MenuItem = (props) => {
     const { category } = useParams();
-    const { isEnglish } = useContext(UserContext);
-
-    // State for handling image loading
-    const [imageLoaded, setImageLoaded] = useState(false);
-
+    
     return (
+        
         <div className="menu-item">
             <Link to={`/menu/${category}/${props.title}`}>
-                <div className="foodimage">
-                    {/* Placeholder image */}
-                    {!imageLoaded && (
-                        <img
-                            src={placeholderImage} // Use the imported placeholder image
-                            alt="Placeholder"
-                            className="placeholder-image"
-                        />
-                    )}
-                    {/* Actual image */}
-                    <img
-                        src={props.image}
-                        alt={props.title}
-                        loading="lazy"
-                        className={`actual-image ${imageLoaded ? 'loaded' : ''}`}
-                        onLoad={() => setImageLoaded(true)}
-                    />
-                </div>
+            <div className="foodimage"><img src={props.image}/></div>
             </Link>
             <div className="item-title">
-                <h3 className="menu-item-title">{!isEnglish && props.altname ? props.altname : props.title}</h3>
-                <p className="menu-item-price">
-                    <h4>${props.price}</h4>
-                </p>
+                <h3 className="menu-item-title">{props.title}</h3>
+                <p className="menu-item-price"> <h4> ${props.price}</h4></p>
             </div>
-            <br />
+            <br/>
             <p className="menu-item-description">{props.description}</p>
         </div>
     );
-};
+}
 
 export default MenuItem;
